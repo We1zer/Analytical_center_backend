6ed23ede63e5e1b557617bf0bdb7f9399c8d42bd
Konopka <taras.konopka2@gmail.com>
2024-07-09 17:43:54 +0300

    fix some changes in server file

--- server.js
+++ server.js
@@ -1,13 +1,38 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const colors = require('colors');
+const logger = require("./middleware/logger");
+const connectDB = require('./config/db');
 
 //Load env vars
 dotenv.config({path: './config/config.env'});
 
+//Connect to database
+connectDB();
+
+//Route files
+const security = require('./routes/security')
+
+
 const app = express();
  
+//Body parser
+app.use(express.json())
+
+app.use(logger);
+//Mout routers
+app.use('/api/v1/securities', security)
+app.use('/api/v1/security', security)
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+const server = app.listen(PORT, () => {
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold);
 });
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    //Close server & exit process
+    server.close( () => process.exit(1));
+})
