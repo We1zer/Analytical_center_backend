0c3afe0c3de43d52175ff3473aea0c1b57e2e13e
We1zer <103381725+We1zer@users.noreply.github.com>
2024-07-23 01:22:43 +0300

    Update recommendation.js

--- routes/recommendation.js
+++ routes/recommendation.js
@@ -4,7 +4,7 @@ const { getInvestmentRecommendations, getInvestmentPerformance } = require('../c
 const router = express.Router();
 const { protect, authorize } = require('../middleware/auth');
 
-router.route('/').get(protect, authorize('user', 'admin'), getInvestmentRecommendations);
+router.route('/').get(getInvestmentRecommendations);
 router.route('/performance').get(protect, authorize('user', 'admin'), getInvestmentPerformance);
 
 module.exports = router;
